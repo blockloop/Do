ed37de48f3dd89c275e562a46ef7f7d1b26aebe3
Brett Jones <bjones027@gmail.com>
2013-06-03 15:37:08 -0500

    working brain and add default brain if main fails

--- scripts/app.js
+++ scripts/app.js
@@ -1,33 +1,22 @@
-global.BrainFile = "db.json";
+global.BrainFile = "./db.json";
 global.Brain = {};
 
 global.persistBrain = function() {
 	require("fs").writeFile(global.BrainFile, JSON.stringify(global.Brain), function(err) {
 		if (err) {
 			console.log("ERROR!");
-			return console.log(err);
+			console.log(err);
 		} else {
-			return console.log("DB successfully written.");
+			console.log("DB successfully written.");
 		}
 	});
 };
 
 global.loadBrain = function() {
-	var fs = require('fs'),
-		data = {};
-
-	fs.readFile(global.BrainFile, "utf8", function(err, fileContents) {
-		if (err) {
-			console.dir("ERROR!");
-			console.dir(err);
-		} else {
-			data = JSON.parse(fileContents);
-		}
-	});
-
-	return data;
+	try {
+		return require(global.BrainFile);
+	} catch(e){}
+	return require('./db.default.json');
 };
 
-global.Brain = global.loadBrain() || {};
-global.Brain.Archive = [];
-global.Brain.Todos = [];
+global.Brain = global.loadBrain();
