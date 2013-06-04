32f18e8bf1792a8a818a6b284ae3710be8882bbf
Brett Jones <bjones027@gmail.com>
2013-06-04 10:08:44 -0500

    switch to localStorage for storage

--- scripts/app.js
+++ scripts/app.js
@@ -1,31 +1,16 @@
-global.BrainFile = "./brain.json";
-global.Brain = {};
+global.Brain = {Archive: [], Todos: []};
 
 global.persistBrain = function() {
-	require("fs").writeFile(global.BrainFile, JSON.stringify(global.Brain), function(err) {
-		if (err) {
-			console.log("ERROR!");
-			console.log(err);
-		} else {
-			console.log("DB successfully written.");
-		}
-	});
+	localStorage.setItem('brain',JSON.stringify(global.Brain));
 };
 
 global.loadBrain = function() {
-	try {return require(global.BrainFile); } catch(e){}
+	try {return JSON.parse(localStorage.getItem('brain')); } catch(e){}
 	return {Archive: [], Todos: []};
 };
 
 global.strHash = function(str) {
-	var hash = 0;
-	if (str.length == 0) return hash;
-	for (i = 0; i < str.length; i++) {
-		char = str.charCodeAt(i);
-		hash = ((hash<<5)-hash)+char;
-		hash = hash & hash; // Convert to 32bit integer
-	}
-	return hash;
+	return str.split("").reduce(function(a,b){a=((a<<5)-a)+b.charCodeAt(0);return a&a},0);
 };
 
 global.Brain = global.loadBrain();
